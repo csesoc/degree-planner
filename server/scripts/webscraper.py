161769158793886d3849aa67a7e8e93d14043b8b
Jeremy Lim <jeremy.lim1636@gmail.com>
2019-08-24 19:19:59 +1000

    Description functionality and started on specialisations

--- server/scripts/webscraper.py
+++ server/scripts/webscraper.py
@@ -7,7 +7,14 @@ from bs4 import BeautifulSoup
 
 # Started by Jeremy Lim on 20/07/2019
 
-# COURSES
+# TO-DO LIST
+# - Get timetable done
+# - Get GenEd done
+# - Majors
+# - Degrees
+# - Add to database
+
+##### COURSES #####
 
 # To print the info of each course
 def printInfo(code, link, name, cred):
@@ -17,71 +24,102 @@ def printInfo(code, link, name, cred):
     print("CREDITS: " + cred) # prints the credits of the course
 
 # To go through each letter's links for courses
-alphabet = ['A','B','C','D','E','F','G','H','I','L','M','N','O','P','R','S','T','V','Y','Z']
+def run_course():
+    alphabet = ['A','B','C','D','E','F','G','H','I','L','M','N','O','P','R','S','T','V','Y','Z']
 
-for letter in alphabet[0:5]:
-    url = 'http://legacy.handbook.unsw.edu.au/vbook2018/brCoursesByAtoZ.jsp?StudyLevel=Undergraduate&descr=' + letter
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    # soup = BeautifulSoup(p_instance_url, "html.parser")
+    for letter in alphabet[0:2]:
 
-    tr = soup.find_all('tr') # this finds the first instance
+        # runs the url for the letter search
+        course_url = 'http://legacy.handbook.unsw.edu.au/vbook2018/brCoursesByAtoZ.jsp?StudyLevel=Undergraduate&descr=' + letter
+        response = requests.get(course_url)
+        course_soup = BeautifulSoup(response.text, "html.parser")
+        # soup = BeautifulSoup(test_url, "html.parser")
 
-    for i in range(1,4):
-        td = tr[i].find_all('td') # this finds each of the td in tr
-        # print(td)
-        code = td[0].contents[0]
-        link = td[1].find_all('a')[0]['href'] # searches for the link
-        name = td[1].find_all('a')[0].contents[0] # searches for the a tags
-        cred = td[2].contents[0] # gets the credits
+        tr = course_soup.find_all('tr') # this finds the first instance
 
-        # DEBUGGING FOR MAIN INFO
-        printInfo(code, link, name, cred)
+        for i in range(1,3):
+            counter = 0
+            td = tr[i].find_all('td') # this finds each of the td in tr
+            # print(td)
+            code = td[0].text
+            link = td[1].find_all('a')[0]['href'] # searches for the link
+            name = td[1].find_all('a')[0].text # searches for the a tags
+            cred = td[2].text # gets the credits
 
-        # DEBUGGING FOR LINK INFO
-        linkyboi = requests.get(link)
-        linkysoup = BeautifulSoup(linkyboi.text, "html.parser")
+            # DEBUGGING FOR MAIN INFO
+            printInfo(code, link, name, cred)
 
-        p_data = linkysoup.find_all('p')
-        for p_instance in p_data:
-            search = p_instance.findChildren()
-            if (search[0].contents[0] == 'Faculty:'):
-                if (len(search) > 1):
-                    print("FACULTY: " + search[1].contents[0])
-                else:
-                    print("FACULTY: " + p_instance.contents[1].strip())
+            # goes to the course link and scrapes the data
+            link_url = requests.get(link)
+            link_soup = BeautifulSoup(link_url.text, "html.parser")
 
-            if (search[0].contents[0] == 'School:'):
-                if (len(search) > 1):
-                    print("SCHOOL: " + search[1].contents[0])
-                else:
-                    print("SCHOOL: " + p_instance.contents[1].strip())
+            p_data = link_soup.find_all('p')
+            h_data = link_soup.find_all('h2')
 
-            # doesn't work yet
-            if (search[0].contents[0] == 'Career:'):
-                if (len(search) > 1):
-                    print("CAREER: " + search[1].contents[0])
-                else:
-                    print("CAREER: " + p_instance.contents[1].strip())
+            # collects course data
+            for p_instance in p_data:
+                search = p_instance.findChildren()
+                # print("LENGTH: " + str(len(search)))
+                if (len(search) > 0 and len(search[0].contents) > 0):
+                    if (search[0].text == 'Faculty:'):
+                        if (len(search) > 1):
+                            print("FACULTY: " + search[1].text)
+                        else:
+                            print("FACULTY: " + p_instance.contents[1].strip())
+
+                    if (search[0].text == 'School:'):
+                        if (len(search) > 1):
+                            print("SCHOOL: " + search[1].text)
+                        else:
+                            print("SCHOOL: " + p_instance.contents[1].strip())
+
+                    if (search[0].text == 'Career:'):
+                        if (len(search) > 1):
+                            print("CAREER: " + search[1].text)
+                        else:
+                            print("CAREER: " + p_instance.contents[1].strip())
+
+                    # GenEd not working yet
+
+                    if (search[0].text == "Available for General Education:"):
+                        if (len(search) > 1):
+                            counter += 1
+                        break
+
+            # collects course descripter
+            for h_instance in h_data:
+                if (h_instance.text == "Description"):
+                    desc_tags = str(h_instance.find_next_siblings()[0])
+                    desc = str(re.sub("<.*?>", "", desc_tags))
+                    print("DESCRIPTION: " + desc)
+
+            # checks for General Education existence in course link
+            if (counter == 0):
+                print("GENED: False")
                 print("")
-                break
+            else:
+                print("GENED: True")
+                print("")
 
-            # doesn't work yet
+##### SPECIALISATIONS (WIP) #####
 
-            # print(search)
-            # prereq = p_instance.find(text=re.compile('Prerequisite: '))
-            # if (prereq):
-            #     # print(faculty)
-            #     # print(school)
-            #     # print(career)
-            #     # the above ones doesn't work yet
-            #     print(prereq)
-            #     print("")
-            #     break
+def run_spec():
+    alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','V','W']
 
-# test stuff
-# for i in range(0,len(soup.find_all('tr'))): # 'a' tags are for links
-#     print("entering loop")
-#     one_a_tag = soup.find_all('tr')[i] # is an array and iterates through each object
-#     print(one_a_tag['href']) # prints the href link
-#     print(one_a_tag.contents[0]) # prints the body content
+    for letter in alphabet[0:2]:
+        spec_url = 'http://legacy.handbook.unsw.edu.au/vbook2018/brSpecialisationsByAtoZ.jsp?StudyLevel=Undergraduate&descr=' + letter
+        response = requests.get(spec_url)
+        spec_soup = BeautifulSoup(response.text, "html.parser")
+
+        spec_tr = spec_soup.find_all('tr') # this finds the first instance
+
+        for i in range(1,3):
+            counter = 0
+            spec_td = spec_tr[i].find_all('td') # this finds each of the td in tr
+            spec_name = spec_td[0].text
+            spec_link = spec_td[0].find_all('a')[0]['href']
+            print(spec_name)
+            print(spec_link)
+            print("")
+
+run_course()
