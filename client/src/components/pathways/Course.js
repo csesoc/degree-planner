8de1e25e2b0bb0740a46aa4138d27bc89f7a93ae
Jonathan Charles <jcharles8246@gmail.com>
2019-09-26 01:58:14 -0400

    Added react-arrow impl, messy css added; logic to translate from input course to dependency linkage complete

--- client/src/components/pathways/Course.js
+++ client/src/components/pathways/Course.js
@@ -1,15 +1,39 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 300
+  },
+  name: {
+    marginBottom: '12px',
+  },
+});
 
 export default function Course(props) {
 
+    const classes = useStyles();
+
     return(
-      <Card>
+      <Card className={classes.card}>
         <CardContent>
+          <Typography variant="h5" component="h2">
             {props.data.code}
+          </Typography>
+          <Typography className={classes.name} color="textSecondary">
+            {props.data.name}
+          </Typography>
         </CardContent>
+        <CardActions>
+          <a href={props.data.outline} target="_blank">
+            <Button size="small">Course Outline</Button>
+          </a>
+        </CardActions>
       </Card>
     );
 }
--- client/src/components/pathways/Pathways.js
+++ client/src/components/pathways/Pathways.js
@@ -1,20 +1,31 @@
 import React, { useState, useEffect } from 'react';
+import Grid from '@material-ui/core/Grid';
+import { ArcherContainer, ArcherElement } from 'react-archer';
 import Typography from '@material-ui/core/Typography';
 import NavBar from '../common/NavBar';
 import Course from './Course';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
-
+  grid: {
+  },
+  gridItem: {
+    padding: "1%",
+  },
+  courseCard: {
+  }
 });
 
 export default function Pathways() {
 
     const classes = useStyles();
+    const [course, setCourse] = useState({});
+    const [prereqs, setPrereqs] = useState([]);
+    const [postreqs, setPostreqs] = useState([]);
     const [courses, setCourses] = useState({});
     const [courseCards, setCourseCards] = useState([]);
 
-    // On Component mounting request course data from server
+    // On Component mounting request course data from server and then transform into the cards
     useEffect(() => {
       async function fetchData() {
         const response = await fetch('/api/courses')
@@ -22,23 +33,116 @@ export default function Pathways() {
            .then(res => setCourses(res))
            .catch((error) => console.log(error.message));
       }
+
+      async function fetchReqs(code) {
+        const response = await fetch('/api/relations/' + code)
+          .then(res => res.json())
+          .then(res => {
+            for (let [key, val] of Object.entries(res.relations)) {
+              if (val.type !== 'prerequisite') {
+                continue
+              }
+              if (val.destination === code) {
+                let srcCourse = findCourse(val.source);
+                if (srcCourse === undefined) {
+                    continue
+                } else {
+                  setPostreqs(postreqs => [...postreqs, srcCourse]);
+                }
+              } else if (val.source === code) {
+                let destCourse = findCourse(val.destination);
+                if (destCourse === undefined) {
+                  continue
+                } else {
+                  setPrereqs(prereqs => [...prereqs, destCourse]);
+                }
+              }
+            }
+          });
+      }
+        
       if (!Object.keys(courses).length) {
         fetchData();
       } else {
         if (!courseCards.length) {
           setCourseCards(courses["courses"].map((courseObj) => {
-            return (
-              <Course data={courseObj} /> 
-            );
+            return courseObjToCard(courseObj);
           }));
+          setCourse(findCourse('COMP2041'));
+          fetchReqs('COMP2041');
         }
       }
-    }, [courses, courseCards]); 
+    }, [course, courses, courseCards, prereqs, postreqs, findCourse]); 
 
-    return(
-      <div>
-        <NavBar />
-        {courseCards}
-      </div>
-    );
+    function findCourse(code) {  
+      return courses["courses"].find(course => course.code === code);
+    };
+
+    function courseObjToCard(courseObj) {
+      return (<Course className={classes.courseCard} key={courseObj.code} data={courseObj} />);
+    };
+
+    function courseObjToPrereqArcher(courseObj) {
+      let rel = {
+        targetId: course.code,
+        targetAnchor: 'left',
+        sourceAnchor: 'right',
+      };
+      return (
+        <ArcherElement key={courseObj.code} id={courseObj.code} relations={[rel]}>
+          {courseObjToCard(courseObj)}
+        </ArcherElement>
+      );
+    };
+
+    function courseObjToPostReqArcher(courseObj) {
+      return (
+        <ArcherElement key={courseObj.code} id={courseObj.code}>
+          {courseObjToCard(courseObj)}
+        </ArcherElement>
+      );
+    }
+
+    function courseToSelectedCourseArcher() {
+      let postReqRelations = postreqs.map(obj => {
+        return ({
+          targetId: obj.code,
+          targetAnchor: 'left',
+          sourceAnchor: 'right'
+        });
+      });
+      return (
+        <ArcherElement id={course.code} relations={postReqRelations}>
+          {courseObjToCard(course)}
+        </ArcherElement>
+      );
+    }
+
+    // Have to do this because if react-archer renders this before it has code to work off, arrows don't generate properly forever.
+    if (course.code !== undefined) {
+      return(
+        <div>
+          <NavBar />
+          <ArcherContainer>
+            <Grid container className={classes.grid}  direction="row" justify="center" alignItems="center" alignContent="center">
+              <Grid item className={classes.gridItem} xs={4} justify="center">
+                {prereqs.map(obj => courseObjToPrereqArcher(obj))}
+              </Grid>
+              <Grid item className={classes.gridItem} xs={4} justify="center">
+                  {courseToSelectedCourseArcher()}
+              </Grid>
+              <Grid item className={classes.gridItem} xs={4} justify="center">
+                  {postreqs.map(obj => courseObjToPostReqArcher(obj))}
+              </Grid>
+            </Grid>
+          </ArcherContainer>
+        </div>
+      );
+    } else {
+      return(
+        <div>
+          <NavBar />
+        </div>
+      );
+    }
 }
