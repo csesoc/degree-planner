984a825dd4a51cd44f904c43abff26d1d542eeb0
bofeiw <bofei@mail.com>
2019-10-21 00:29:08 +1100

    add an empty search bar to planner page

--- client/src/components/planner/DegreePlanner.js
+++ client/src/components/planner/DegreePlanner.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import Select from "react-select";
+import Footer from "../common/Footer";
+import {makeStyles} from "@material-ui/core";
+
+const useStyles = makeStyles({
+    searchBar: {
+        width: "80%",
+        margin: "auto",
+        paddingTop: "2%"
+
+    }
+});
 
 export default function DegreePlanner() {
+    const classes = useStyles();
 
     return (
         <div>
-          <h1>Degree Planner</h1>
+            <h1>Degree Planner</h1>
+            <Select
+                className={classes.searchBar}
+                isSearchable
+                // isLoading={!Object.keys(courses).length}
+                // options={courseOptions}
+                // onChange={handleSearchValueChange}
+            />
+            <Footer/>
         </div>
     );
 }
