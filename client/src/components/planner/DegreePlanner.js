1e52520ff4668701c07ae4987d399bce16e64805
bofeiw <bofei@mail.com>
2019-10-25 01:49:57 +1100

    selectable search bar is added to planner - yay! remove redundant footer

--- client/src/components/planner/DegreePlanner.js
+++ client/src/components/planner/DegreePlanner.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Select from "react-select";
-import Footer from "../common/Footer";
 import {makeStyles} from "@material-ui/core";
 
 const useStyles = makeStyles({
@@ -8,12 +7,48 @@ const useStyles = makeStyles({
         width: "80%",
         margin: "auto",
         paddingTop: "2%"
-
     }
 });
 
 export default function DegreePlanner() {
     const classes = useStyles();
+
+    const [course, setCourse] = useState({});
+    const [prereqs, setPrereqs] = useState([]);
+    const [postreqs, setPostreqs] = useState([]);
+
+    const [courses, setCourses] = useState([]);
+    const [courseOptions, setCourseOptions] = useState([]);
+
+    useEffect(() => {
+        if (!Object.keys(courses).length) {
+            fetch('/api/courses')
+                .then(res => res.json())
+                .then(res => {
+                    setCourses(res);
+                    setCourseOptions(res.courses.map((obj) => {
+                        return ({
+                            "label": obj.code,
+                            "value": obj,
+                        });
+                    }));
+                })
+                .catch((error) => console.log(error.message));
+        } else if (Object.keys(course).length) {
+            fetch('/api/relations/' + course.code)
+                .then(res => res.json())
+                .then(res => {
+                    // course info retrieved successfully here
+                    console.log(res)
+                })
+        }
+    }, [course, setCourse, courses]);
+
+    const handleSearchValueChange = value => {
+        setCourse(value.value);
+        setPrereqs([]);
+        setPostreqs([]);
+    };
 
     return (
         <div>
@@ -21,11 +56,10 @@ export default function DegreePlanner() {
             <Select
                 className={classes.searchBar}
                 isSearchable
-                // isLoading={!Object.keys(courses).length}
-                // options={courseOptions}
-                // onChange={handleSearchValueChange}
+                isLoading={!Object.keys(courses).length}
+                options={courseOptions}
+                onChange={handleSearchValueChange}
             />
-            <Footer/>
         </div>
     );
 }
