d7c602db38ba8335d9a8fa49b08c653a51017c52
xLilianW <z5204849@student.unsw.edu.au>
2019-10-25 16:43:20 +1100

    Added multiselects to minors and majors. Added deletable chips

--- client/src/components/planner/DegreeInputForm.js
+++ client/src/components/planner/DegreeInputForm.js
@@ -8,6 +8,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Input from '@material-ui/core/Input';
 import Checkbox from '@material-ui/core/Checkbox';
 import ListItemText from '@material-ui/core/ListItemText';
+import Chip from '@material-ui/core/Chip';
 
 
 const useStyles = makeStyles({
@@ -15,28 +16,58 @@ const useStyles = makeStyles({
         width: '300px',
         padding: '0 5%',
         marginTop: "1em"
-    }
-});
+    },
+    chips: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    chip: {
+      margin: 2,
+    }});
 
 export default function DegreeInputForm() {
 
     const classes = useStyles();
     const [degrees, setDegrees] = React.useState([]);
-    const [major, setMajor] = React.useState([]);
-    const [minor, setMinor] = React.useState([]);
+    const [majors, setMajors] = React.useState([]);
+    const [minors, setMinors] = React.useState([]);
 
 
-    const handleDegreeChange = event => {
+    function handleDegreeChange(event) {
       setDegrees(event.target.value);
     };
 
+    function handleDeleteDegree(value) {
+      let updated = degrees.filter((curr, index, arr) => {
+        return (curr != value)
+      })
+      
+      setDegrees(updated);
+    }
+
     function handleMajorChange(event) {
-      setMajor(event.target.value);
+      setMajors(event.target.value);
     };
 
+    function handleDeleteMajor(value) {
+      let updated = majors.filter((curr, index, arr) => {
+        return (curr != value)
+      })
+      
+      setMajors(updated);
+    }
+
     function handleMinorChange(event) {
-      setMinor(event.target.value);
+      setMinors(event.target.value);
     };
+
+    function handleDeleteMinor(value) {
+      let updated = minors.filter((curr, index, arr) => {
+        return (curr != value)
+      })
+      
+      setMinors(updated);
+    }
 
     const degreeList = {
       "Bachelor of Engineering (Honours)": 3707,
@@ -56,10 +87,18 @@ export default function DegreeInputForm() {
           <MenuItem key={degree_code} value={degree}>
             <Checkbox checked={degrees.includes(degree)} />
             <ListItemText primary={degree} />
+          </MenuItem>);  
+        let majorListOptions = Object.entries(majorList).map(([degree, degree_code]) => 
+          <MenuItem key={degree_code} value={degree}>
+            <Checkbox checked={majors.includes(degree)} />
+            <ListItemText primary={degree} />
+        </MenuItem>);        
+        let minorListOptions = Object.entries(minorList).map(([degree, degree_code]) => 
+          <MenuItem key={degree_code} value={degree}>
+            <Checkbox checked={minors.includes(degree)} />
+            <ListItemText primary={degree} />
           </MenuItem>);
-        let majorListOptions = Object.entries(majorList).map(([degree, degree_code]) => <MenuItem key={degree_code} value={degree_code}>{degree}</MenuItem>);
-        let minorListOptions = Object.entries(minorList).map(([degree, degree_code]) => <MenuItem key={degree_code} value={degree_code}>{degree}</MenuItem>);
-
+        
         return (
             <form className={classes.form} autoComplete="off">
               <Grid container spacing={3}>
@@ -70,9 +109,14 @@ export default function DegreeInputForm() {
                       multiple
                       value={degrees}
                       onChange={handleDegreeChange}
-                      input={<Input id="select-multiple" />}
-                      renderValue={selected => selected.join(', ')}
-                      inputProps={{
+                      input={<Input id="select-multiple-chip" />}
+                      renderValue={selected => (
+                        <div className={classes.chips}>
+                          {selected.map(value => (
+                            <Chip onDelete={() => {handleDeleteDegree(value)}} key={value} label={value} className={classes.chip} />
+                          ))}
+                        </div>
+                      )}                      inputProps={{
                         name: 'Degree',
                         id: 'degree',
                       }}
@@ -85,8 +129,16 @@ export default function DegreeInputForm() {
                   <FormControl fullWidth className="major_selector_formcontrol">
                     <InputLabel htmlFor="major">Major</InputLabel>
                     <Select
-                      value={major}
-                      onChange={handleMajorChange}
+                      multiple
+                      value={majors}
+                      input={<Input id="select-multiple-chip" />}
+                      renderValue={selected => (
+                        <div className={classes.chips}>
+                          {selected.map(value => (
+                            <Chip onDelete={() => {handleDeleteMajor(value)}} key={value} label={value} className={classes.chip} />
+                          ))}
+                        </div>
+                      )}                       onChange={handleMajorChange}
                       inputProps={{
                         name: 'Major',
                         id: 'major',
@@ -101,8 +153,16 @@ export default function DegreeInputForm() {
                   <FormControl fullWidth className="minor_selector_formcontrol">
                     <InputLabel htmlFor="minor">Minor</InputLabel>
                     <Select
-                      value={minor}
-                      onChange={handleMinorChange}
+                      multiple
+                      value={minors}
+                      input={<Input id="select-multiple-chip" />}
+                      renderValue={selected => (
+                        <div className={classes.chips}>
+                          {selected.map(value => (
+                            <Chip onDelete={() => {handleDeleteMinor(value)}} key={value} label={value} className={classes.chip} />
+                          ))}
+                        </div>
+                      )}                       onChange={handleMinorChange}
                       inputProps={{
                         name: 'Minor',
                         id: 'minor',
