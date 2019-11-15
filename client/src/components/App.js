ddcb7e4a736858dedc3a62b08567db5ca3f88dbf
Jacqueline Cross <u6421489@anu.edu.au>
2019-11-15 14:39:36 +1100

    yay colours

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/styles';
+import Theme from './common/Theme';
 import Plannify from './planner/Plannify';
 import Pathways from './pathways/Pathways';
 
 const App = () => {
     return (
-      <div className="App">
-        <Switch>
-          <Route exact path='/' component={Plannify}/>
-          <Route exact path='/pathways' component={Pathways}/>
-        </Switch>
-      </div>
+      <ThemeProvider theme={Theme}>
+        <div className="App">
+          <Switch>
+            <Route exact path='/' component={Plannify}/>
+            <Route exact path='/pathways' component={Pathways}/>
+          </Switch>
+        </div>
+     </ThemeProvider>
     );
 }
 
