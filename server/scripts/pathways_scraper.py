8ad534557dd71983c60159cef5f0a8e9f128cf4a
ohxeighty <andrew.yu6@hotmail.com>
2020-11-01 20:06:14 +1100

    New pathways scraper. Will pull handbook listings and populate DB later.

--- server/scripts/pathways_scraper.py
+++ server/scripts/pathways_scraper.py
@@ -1,220 +1,42 @@
-#!/usr/bin/python3
+import requests
+from bs4 import BeautifulSoup 
 
-'''
-   Copyright 2019 UNSW CSESoc
+class course:
+    def __init__(self, course_type, ):
+        # Post, Under, Research
+        self.type = course_type
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
 
-     http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-'''
 
-import codecs, os, re, requests, sys, pg8000
-from requests.exceptions import HTTPError
+# A bit roundabout, but we grab the handbook page from the timetable listing since its nice and static and not react...
 
-COURSE_LIST_RE = re.compile(r'<TD class="(?:evenTableCell)?" align="left">([A-Z]{4}[0-9]{4})</TD>')
-PREREQS_RE = re.compile(r"Pre-?req(?:uisites?)?:(.*?)(?:</p>|;)")
-EXCLUSIONS_RE = re.compile(r"((?:Excluded|Exclusion|Exclusions|(?:and )?Excludes)[: ](.*?))(?:</p>|<br />)", re.IGNORECASE)
-COREQS_RE = re.compile(r"Co-?requisite:(.*?)</p>", re.IGNORECASE)
-NAME_RE = re.compile(r"<title>UNSW Handbook Course - (.*?) - [A-Z]{4}[0-9]{4}</title>", re.DOTALL)
-DESC_RE = re.compile(r"<!-- Start Course Description -->(.*?)<!-- End Course description -->", re.DOTALL | re.IGNORECASE)
-GENED_RE = re.compile(r"Available for General Education:")
-OUTLINE_RE = re.compile(r"Course Outline:.*?<a .*?href=[\"'](.*?)[\"']")
-UOC_RE = re.compile(r"Units of Credit:.*?([0-9]+)")
-COURSE_RE = re.compile(r"[A-Z]{4}[0-9]{4}", re.IGNORECASE)
-BR_RE = re.compile(r"<br ?/?>", re.IGNORECASE)
-TAG_RE = re.compile(r"</?.*?>")
+base_url =  "http://timetable.unsw.edu.au/2021/"
 
-TYPE_PREREQUISITE = "prerequisite"
-TYPE_COREQUISITE = "corequisite"
-TYPE_EXCLUSION = "exclusion"
+# Similar to the 2018 handbook AtoZ listing
+listing_by_category_url = base_url + "subjectSearch.html"
 
-COURSES_DIR = "courses"
-UG = "undergraduate"
-PG = "postgraduate"
-CURRENT_YEAR = "2018"
+# Grab the top level listing of all the subjects 
+top_level_listing = requests.get(listing_by_category_url) 
+soup = BeautifulSoup(top_level_listing.text, "html.parser")
 
-def scrape_list(url):
-    
-    print("Fetching page data")
-    try:
-        data = requests.get(url).text
-        print("Finding course codes")
-        codes = re.findall(COURSE_LIST_RE, data)
-        print("Done")
-        return codes
-    except HTTPError as http_err:
-        print("HTTP error")
-    except Exception as err:
-        print(err)
-    return None
 
-def scrape_area(area, level=UG):
-    print("Finding all courses for " + str(area))
-    return scrape_list("http://legacy.handbook.unsw.edu.au/vbook" + CURRENT_YEAR + "/brCoursesBySubjectArea.jsp?studyArea=" + str(area) + "&StudyLevel=" + str(level))
+# Grab all the rows with links to the subject pages
+rows = soup.find_all("tr", {"class": ["rowLowlight","rowHighlight"]})
 
-def scrape_everything(level):
-    url = "http://legacy.handbook.unsw.edu.au/vbook%s/brCoursesBySubjectArea.jsp?StudyLevel=%s&descr=A" % (CURRENT_YEAR, level)
-    print("Reading area list")
-    data = requests.get(url).text
-    codes = re.findall(r'>([A-Z]{4}): .*?</A></TD>', data)
-    print(codes)
-    for code in codes:
-        for course in scrape_area(code, level):
-            scrape(course, level)
+# Grab links to the subject pages 
+subject_links = [row.find("a").get("href") for row in rows]
 
-def scrape(course, level=UG):
-    url = "http://legacy.handbook.unsw.edu.au/%s/courses/%s/%s.html" % (level, CURRENT_YEAR, course)
-    filename = "%s/%s.html" % (COURSES_DIR, course)
-    if os.path.exists(filename):
-        print("Skipping " + course)
-        return
-    print("Fetching " + course)
-    try:
-        data = requests.get(url).text
-    except Exception as e:
-        print("FAILED: " + e.message)
-        return
-    with open(filename, "w") as f:
-        f.write(data)
+# ['ACCTKENS.html', 'ACTLKENS.html', 'AEROKENS.html', 'ANATKENS.html', 'ARCHKENS.html', 'ARTSKENS.html', 'ASIAKENS.html', 'ATSIKENS.html', 'AUSTKENS.html', 'AVENKENS.html', 'AVIAKENS.html', 'AVIFKENS.html', 'AVIGKENS.html', 'BABSKENS.html', 'BEESKENS.html', 'BEILKENS.html', 'BENVKENS.html', 'BINFKENS.html', 'BIOCKENS.html', 'BIOMKENS.html', 'BIOSKENS.html', 'BIOTKENS.html', 'BLDGKENS.html', 'CDEVKENS.html', 'CEICKENS.html', 'CHEMKENS.html', 'CHENKENS.html', 'CLIMKENS.html', 'CODEKENS.html', 'COMDKENS.html', 'COMMKENS.html', 'COMPKENS.html', 'CONSKENS.html', 'CRIMKENS.html', 'CRTVKENS.html', 'CVENKENS.html', 'DATAKENS.html', 'DESNKENS.html', 'DPBSKENS.html', 'DPGEKENS.html', 'DPSTKENS.html', 'ECONKENS.html', 'EDSTKENS.html', 'ELECKENS.html', 'ENGGKENS.html', 'ENGLKENS.html', 'ENVSKENS.html', 'EXCHKENS.html', 'FINSKENS.html', 'FOODKENS.html', 'GENCKENS.html', 'GENEKENS.html', 'GENLKENS.html', 'GENMKENS.html', 'GENSKENS.html', 'GENYKENS.html', 'GEOLKENS.html', 'GEOSKENS.html', 'GMATKENS.html', 'GSBEKENS.html', 'GSOEKENS.html', 'HDATKENS.html', 'HESCKENS.html', 'HISTKENS.html', 'HUMLKENS.html', 'HUMSKENS.html', 'IDESKENS.html', 'IESTKENS.html', 'INFSKENS.html', 'INSTKENS.html', 'INTAKENS.html', 'INTDKENS.html', 'JURDKENS.html', 'LANDKENS.html', 'LAWSKENS.html', 'LINGKENS.html', 'MANFKENS.html', 'MARKKENS.html', 'MATHKENS.html', 'MATSKENS.html', 'MBAXKENS.html', 'MDCNKENS.html', 'MDIAKENS.html', 'MECHKENS.html', 'MEREKENS.html', 'MFACKENS.html', 'MFINKENS.html', 'MGMTKENS.html', 'MICRKENS.html', 'MINEKENS.html', 'MMANKENS.html', 'MNGTKENS.html', 'MODLKENS.html', 'MSCIKENS.html', 'MTRNKENS.html', 'MUSCKENS.html', 'NANOKENS.html', 'NCHRKENS.html', 'NEURKENS.html', 'OBSTKENS.html', 'OPTMKENS.html', 'PATHKENS.html', 'PHARKENS.html', 'PHCMKENS.html', 'PHILKENS.html', 'PHOPKENS.html', 'PHSLKENS.html', 'PHTNKENS.html', 'PHYSKENS.html', 'PLANKENS.html', 'PLTXKENS.html', 'POLSKENS.html', 'POLYKENS.html', 'PPECKENS.html', 'PSCYKENS.html', 'PSYCKENS.html', 'PTRLKENS.html', 'REGZKENS.html', 'RESTKENS.html', 'RISKKENS.html', 'SCIFKENS.html', 'SENGKENS.html', 'SLSPKENS.html', 'SOCAKENS.html', 'SOCFKENS.html', 'SOCWKENS.html', 'SOLAKENS.html', 'SOMSKENS.html', 'SOSSKENS.html', 'SPRCKENS.html', 'SRAPKENS.html', 'STAMKENS.html', 'SURGKENS.html', 'SUSDKENS.html', 'SWCHKENS.html', 'TABLKENS.html', 'TELEKENS.html', 'UDESKENS.html', 'VISNKENS.html', 'WOMSKENS.html', 'YENGKENS.html', 'YMEDKENS.html', 'ZZBUKENS.html', 'ZZENKENS.html', 'ZZSCKENS.html', 'ADADCOFA.html', 'DARTCOFA.html', 'DDESCOFA.html', 'SAHTCOFA.html', 'SARTCOFA.html', 'SDESCOFA.html', 'SOMACOFA.html', 'YCANADFA.html', 'ZBUSADFA.html', 'ZEITADFA.html', 'ZGENADFA.html', 'ZHSSADFA.html', 'ZINTADFA.html', 'ZPEMADFA.html', 'ZZCAADFA.html']
 
-if __name__ == "__main__":
-    if not os.path.exists(COURSES_DIR):
-        os.mkdir(COURSES_DIR)
-    scrape_everything(UG)
-    scrape_everything(PG)
-    
-    # Database Construction
-    conn = pg8000.connect(database="degree_planner", port=5432, host="localhost", user="postgres", password="sroot")
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS pathways_courses (code text primary key, name text, description text, prerequisites text, corequisites text, exclusions text, gened integer, outline text, uoc integer)")
-    cur.execute("CREATE TABLE IF NOT EXISTS pathways_relationships (source text, destination text, type text)")
-    conn.commit()
+# Grab courses from each subject 
+course_links = [] 
+for subject in subject_links: 
+    print(subject)
+    subject_page = requests.get(base_url+subject)
+    subject_soup = BeautifulSoup(subject_page.text)
+    course_rows = subject_soup.find_all("tr", {"class": ["rowLowlight", "rowHighlight"]})
+    course_links += [row.find("a").get("href") for row in course_rows]
+    print(course_links)
 
-    print("\nLoading course list")
-    filenames = os.listdir(COURSES_DIR)
-    i = 0
-    for filename in filenames:
-        i += 1
-        code = filename.rstrip(".html")
-        print("Reading %s (%d/%d)" % (code, i, len(filenames)))
-
-        # open with unicode support
-        f = codecs.open("%s/%s" % (COURSES_DIR, filename), encoding="utf-8", mode="r")
-        data = f.read()
-        f.close()
-
-        # strip &nbsp;'s and <strong> tags
-        data = data.replace("&nbsp;", " ")
-        data = data.replace("<strong>", "")
-        data = data.replace("</strong>", "")
-
-        # find name
-        match = re.search(NAME_RE, data)
-        if match:
-            name = match.group(1).strip().replace("\n", "")
-            print("Found name:", name)
-        else:
-            name = None
-            print("Failed to find name for course: " + str(filename))
-            continue
-
-        # find exclusions. all of them.
-        exclusions = ""
-        exclusions_list = []
-        while True:
-            match = re.search(EXCLUSIONS_RE, data)
-            if match:
-                exclusions = match.group(2).strip()
-                print("Found exclusions:", exclusions)
-                data = data.replace(match.group(1), "")
-                exclusions_list = re.findall(COURSE_RE, exclusions)
-                print("Exclusions list:", exclusions_list)
-            else:
-                break
-        # find corequisites
-        match = re.search(COREQS_RE, data)
-        if match:
-            coreqs = match.group(1).strip()
-            print("Found corequisites: ", coreqs)
-            data = data.replace(match.group(0), "")
-            coreqs_list = list(map(str.upper, re.findall(COURSE_RE, coreqs)))
-            print("Corequisites list: ", coreqs_list)
-        else:
-            coreqs = None
-            coreqs_list = []
-            print("Couldn't find corequisites")
-
-        # find prerequisites
-        match = re.search(PREREQS_RE, data)
-        if match:
-            prereqs = match.group(1).strip()
-            print("Found prerequisites: ", prereqs)
-            data = data.replace(match.group(0), "")
-            prereqs_list = list(map(str.upper, re.findall(COURSE_RE, prereqs)))
-            print("Prerequisites list: ", prereqs_list)
-        else:
-            prereqs = None
-            prereqs_list = []
-            print("Couldn't find prerequisites")
-
-        # find description
-        match = re.search(DESC_RE, data)
-        if match:
-            desc = match.group(1).strip()
-            print("Found description: " + str(desc))
-        else:
-            desc = None
-            print("Couldn't find description")
-
-        # find general education statement
-        match = re.search(GENED_RE, data)
-        if match:
-            gened = 1
-        else:
-            gened = 0
-
-        # find course outline
-        match = re.search(OUTLINE_RE, data)
-        if match:
-            outline = match.group(1).strip()
-            print("Found course outline: ", outline)
-        else:
-            outline = None
-            print("Couldn't find course outline")
-
-        # find uoc
-        match = re.search(UOC_RE, data)
-        if match:
-            uoc = match.group(1).strip()
-            try:
-                uoc = int(uoc)
-                print("Found UoC: ", uoc)
-            except:
-                print("UoC was not an integer: '%s'" % uoc)
-                uoc = None
-        else:
-            uoc = None
-            print("Couldn't find UoC")
-
-        course_query, course_args = pg8000.core.convert_paramstyle("qmark", "INSERT INTO pathways_courses (code, name, description, prerequisites, corequisites, exclusions, gened, outline, uoc) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?) ON CONFLICT DO NOTHING")
-        cur.execute(course_query, course_args((code, name, desc, prereqs, coreqs, exclusions, gened, outline, uoc)))
-        for prereq in prereqs_list:
-            prereq_query, prereq_args = pg8000.core.convert_paramstyle("qmark", "INSERT INTO pathways_relationships (source, destination, type) VALUES (?, ?, ?) ON CONFLICT DO NOTHING")
-            cur.execute(prereq_query, prereq_args((code, prereq, TYPE_PREREQUISITE)))
-        for coreq in coreqs_list:
-            coreq_query, coreq_args = pg8000.core.convert_paramstyle("qmark", "INSERT INTO pathways_relationships (source, destination, type) VALUES (?, ?, ?) ON CONFLICT DO NOTHING")
-            cur.execute(coreq_query, coreq_args((code, coreq, TYPE_COREQUISITE)))
-        for exclusion in exclusions_list:
-            exclusions_query, exclusions_args = pg8000.core.convert_paramstyle("qmark", "INSERT INTO pathways_relationships (source, destination, type) VALUES (?, ?, ?) ON CONFLICT DO NOTHING")
-            cur.execute(exclusions_query, exclusions_args((code, exclusion, TYPE_EXCLUSION)))
-        print()
-        conn.commit()
-
+# For each course, scrape the handbook listing
